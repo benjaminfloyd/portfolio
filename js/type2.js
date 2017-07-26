6f0f1b949aa911f44d7c5688b8aa66664620ae58
benjaminfloyd <benjamin.floyd@me.com>
2017-07-25 20:15:50 -0400

    index.html commit

---
+++ js/type2.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded',function(event){
+
+
+  // array with texts to type in typewriter
+  var dataText = [ "I am a full stack software engineer, that focuses on building innovative and creative Full Stack web applications centered on application performance, user experience simplicity and responsive design aesthetic. My developer philosophy is that continuous integration, agile methodologies, and test-driven development lead to better applications."];
+  
+
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.querySelector("about-me").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(17000);
+
+  function type(text, n) {
+  if (n < (text.length)) {
+    $('.test').html(text.substring(0, n+1));
+    n++;
+    setTimeout(function() {
+      type(text, n)
+    }, 100);
+  }
+}
+
+$('.start').click(function(e) {
+  e.stopPropagation();
+  
+  var text = $('.test').data('text');
+  
+  type(text, 0);
+});
+
+
+});
